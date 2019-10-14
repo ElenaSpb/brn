1f57fb1bebe8ac62f29b4e15159e34627026a0cf
Artem Suschev <Suschev.Artem@gmail.com>
2019-10-14 20:27:33 +0300

    added prettier config

---
+++ frontend/.prettierrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+  trailingComma: 'all',
+  overrides: [{
+    files: '*.hbs',
+    options: {
+      semi: true,
+      singleQuote: false,
+      parser: 'glimmer',
+    }
+  }],
+};
